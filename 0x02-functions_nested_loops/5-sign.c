595259e51828d357fbe5c634bf4de6bfc3a16c0a
nelsonwamalwa <wamalwanelson@gmail.com>
2023-06-20 14:28:43 -0700

    Another task five comment correcrion asper betty style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 
 	/**
 	 * main.h - Entry point
-	 * print_sign - prints the sign of a number
+	 * int print_sign - prints the sign of a number
 	 * @n: the int to check
 	 * Return: 1 and prints + if n is greater than zero
 	 * 0 and prints 0 if n is zero
