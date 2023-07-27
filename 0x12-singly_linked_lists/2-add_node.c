8bcbae2dff823020775cd2f748d4fa39b2e6e2fc
nelsonwamalwa <wamalwanelson@gmail.com>
2023-07-27 05:48:10 -0700

    2-add_node.c betty style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_node;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (*head);
 }
