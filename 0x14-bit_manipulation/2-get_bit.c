19ad58bc950893971a99e943bf430a08f61ff8f8
nelsonwamalwa <wamalwanelson@gmail.com>
2023-08-04 10:03:47 -0700

    #include main.h Betty correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *get_bit -This returns the value or series of a bit at an index in a decimal number
- * @n:The number to search
- * @index:Gets the  index of the bit
+ * get_bit -This  returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: The  index of the bit
  *
- * Return:The  value of the bit
+ * Return: The  value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
