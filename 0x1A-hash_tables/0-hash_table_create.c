bac21fecbf5b2fe7ad05d6ad505e5b70fd918694
NelsonWamalwa <wamalwanelson@gmail.com>
2023-10-06 15:44:41 +0300

    Removed Main files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -This Creates a hash table
- * @size: Size of the array
+ * hash_table_create - Creating a hash table
+ * @size: size of given array
  *
- * Return: If error occurs - NULL
- *         Or else - a pointer to the new hash table
+ * Return: Error occurs - NULL
+ *         OR Else - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
